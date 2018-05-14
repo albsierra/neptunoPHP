6e8f4e7a47010f6f8f5a605d0cb9ca5bfd6338bf
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-05-14 12:27:44 +0200

    Crear la consulta y recorrer el resultado con un foreach

--- php/clientesDB.php
+++ php/clientesDB.php
@@ -34,6 +34,19 @@ try {
     $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
     /*** echo a message saying we have connected ***/
 
+    /*** The SQL SELECT statement ***/
+    $sql = "SELECT * FROM Clientes WHERE IdCliente LIKE '%$vIdCliente%' LIMIT 22";
+    foreach ($dbh->query($sql) as $row)
+    { ?>
+        <TR>
+            <TD><?php print $row['IdCliente'] ?></TD>
+            <TD><?php print $row['NombreCompany'] ?></TD>
+            <TD><?php print $row['NombreContacto'] ?></TD>
+            <TD><?php print $row['CargoContacto'] ?></TD>
+        </TR>
+
+    <?php }
+
     /*** close the database connection ***/
     $dbh = null;
 
