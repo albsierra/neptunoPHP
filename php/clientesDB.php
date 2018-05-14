e36cc0be20ddd31cdbbf22c7ade80be64e13b245
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-05-14 12:26:56 +0200

    Crear la conexión a la base de datos de Neptuno.

--- php/clientesDB.php
+++ php/clientesDB.php
@@ -8,7 +8,41 @@
         </TR>
     </thead>
     <tbody>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alberto
+ * Date: 5/04/18
+ * Time: 17:56
+ */
 
+/*** mysql hostname ***/
+$hostname = 'localhost';
+
+/*** mysql dbname ***/
+$dbname = 'Neptuno';
+
+/*** mysql username ***/
+$username = 'root';
+
+/*** mysql password ***/
+$password = 'alumno';
+
+$vIdCliente = isset($_POST['idCliente']) ? $_POST['idCliente'] : '';
+
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+    /*** echo a message saying we have connected ***/
+
+    /*** close the database connection ***/
+    $dbh = null;
+
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+}
+?>
     </tbody>
 </TABLE>
 
