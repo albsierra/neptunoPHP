859e43432b1425b69209690cbcc2307eb251c349
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-05-14 09:12:16 +0200

    Esqueleto inicial de la aplicación de Neptuno

---
+++ php/clientesDB.php
@@ -0,0 +1,14 @@
+<TABLE id="t01">
+    <thead>
+        <TR>
+            <TH>IdCliente</TH>
+            <TH>NombreCompany</TH>
+            <TH>NombreContacto</TH>
+            <TH>CargoContacto</TH>
+        </TR>
+    </thead>
+    <tbody>
+
+    </tbody>
+</TABLE>
+
